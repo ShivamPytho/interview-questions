c058bd0e17c85ff766d177263b55f5d59a976a02
shivam <shivamagrahari0786@gmail.com>
2022-04-07 23:49:50 +0530

    Initial commit

---
+++ max-array.php
@@ -0,0 +1,12 @@
+<?php
+$array = array(
+    'a' => 1,
+    'b' => 3,
+    'c' => 5,
+    'd' => 7,
+    'e' => 9,
+ );
+$result = max($array);
+echo "MaxValue:- ".$result;
+
+?>
---
+++ min-array.php
@@ -0,0 +1,11 @@
+<?php
+$array = array(
+    'a' => 1, 
+    'b' => 4, 
+    'c' => 5, 
+    'd' => 7, 
+    'e' => 8, 
+);
+$result = min($array);
+echo "Min Number :-".$result; 
+?>
